91c93ee7a0a485e4d25e79941f1e108277d25d07
x9cf9w73cv7y <pc@isengard>
2026-01-01 17:09:01 +0100

    add cosmic

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,54 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# Tagged release
+
+dnf5 remove -y xwaylandvideobridge
+
+# Gets the mandatory packages installed in kde-desktop and removes them.
+dnf5 group info kde-desktop | \
+    sed -n '/^Mandatory packages\s*:/,/^\(Default\|Optional\) packages\s*:/ {
+        /^\(Default\|Optional\) packages\s*:/q  # Quit if we hit Default/Optional header
+        s/^.*:[[:space:]]*//p
+    }' | \
+    xargs dnf5 remove -y
+
+#dnf5 install -y \
+#    qt6-qtbase-gui \
+#    qt6-qtdeclarative \
+#    qt6-qtsvg \
+#    qt6-qtwayland \
+#    kf6-kcoreaddons \
+#    kf6-kconfig \
+#    kf6-ki18n \
+#    kf6-kwidgetsaddons \
+#    kf6-kwindowsystem \
+#    kf6-kirigami \
+#    kf6-kiconthemes \
+#    breeze-icon-theme
+
+dnf5 clean all && \
+rm -rf /var/cache/dnf/*
+
+dnf5 group install -y cosmic-desktop cosmic-desktop-apps
+
+dnf5 clean all && \
+rm -rf /var/cache/dnf/*
+
+dnf5 clean all && \
+rm -rf /var/cache/dnf/*
+
+dnf5 install -y @cosmic-desktop-environment
+dnf5 clean all && \
+rm -rf /var/cache/dnf/*
+
+# Other packages I use
+#dnf5 install -y \
+#    neovim \
+#    ncdu \
+#    NetworkManager-tui
+
+systemctl disable display-manager && systemctl enable cosmic-greeter.service -f
 
 # Use a COPR Example:
 #
